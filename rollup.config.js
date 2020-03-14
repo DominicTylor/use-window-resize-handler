0f57093b8659211c2b845c907eadac1229dd96fa
DominicTylor <DominicTylor13@gmail.com>
2020-03-14 23:29:35 +0300

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
+import cleanup from 'rollup-plugin-cleanup';
 
 import pkg from './package.json'
 
@@ -11,13 +13,13 @@ export default {
             file: pkg.main,
             format: 'cjs',
             exports: 'named',
-            sourcemap: true
+            sourcemap: true,
         },
         {
             file: pkg.module,
             format: 'es',
             exports: 'named',
-            sourcemap: true
+            sourcemap: true,
         }
     ],
     plugins: [
@@ -26,6 +28,10 @@ export default {
             rollupCommonJSResolveHack: true,
             clean: true
         }),
-        commonjs()
+        terser(),
+        commonjs(),
+        cleanup({
+            comments: 'none',
+        }),
     ]
 }
